5635ae454a8a291435a1f719eb0a14df7919659e
Akash Vadakkeveetil <akashvadakkeveetil@gmail.com>
2024-12-13 15:15:34 +0530

    initial commit

---
+++ src/main/java/com/akashvadakeveetil/Pill_Point/PillPointApplication.java
@@ -0,0 +1,13 @@
+package com.akashvadakeveetil.Pill_Point;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PillPointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PillPointApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/akashvadakeveetil/Pill_Point/PillPointApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akashvadakeveetil.Pill_Point;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PillPointApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
